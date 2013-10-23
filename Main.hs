38ce6a3ed7c70ba27d0ea61cc0a98c1429d84f4c
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2013-10-23 11:48:19 +0100

    Don't import =<<
    Ignore-this: bc78079349afcd0696d84bf10e9327ff
    
    darcs-hash:20131023104819-04b88-37e332bac7c3a7cc747871604caa23da5878bfc8.gz

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Prelude hiding (map, foldr, (.))
+import Prelude hiding (map, foldr, (.), (=<<))
 import Data.Char (isAlpha, toUpper, isSpace)
 import Helpers (getLines, runTests)
 
