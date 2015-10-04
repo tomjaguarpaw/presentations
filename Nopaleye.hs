5ea0d74e96c908fd198fffec01d68d46f8af6410
Tom Ellis <tom-2013@purelyagile.com>
2015-10-04 12:21:12 +0100

    Nopalye

---
+++ Nopaleye.hs
@@ -0,0 +1,149 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE Arrows #-}
+
+module Nopaleye where
+
+import qualified Control.Foldl as Foldl
+import qualified Data.Profunctor as P
+import qualified Data.Profunctor.Product as PP
+import qualified Control.Applicative as A
+import qualified Control.Arrow as Arr
+import qualified Data.Map as Map
+import qualified Data.Monoid as M
+import           Data.Monoid ((<>))
+import qualified Data.List as L
+
+type Query a = QueryArr () a
+type QueryArr a b = Arr.Kleisli [] a b
+
+restrict :: QueryArr Bool ()
+restrict = Arr.Kleisli (\b -> if b then [()] else [])
+
+data Aggregator a b =
+  forall r r'. Ord r => Aggregator (a -> r) (Foldl.Fold a r') ((r, r') -> b)
+
+instance P.Profunctor Aggregator where
+  dimap f g (Aggregator group fold finish) =
+    Aggregator (P.lmap f group) (P.lmap f fold) (fmap g finish)
+
+instance Functor (Aggregator a) where
+  fmap f (Aggregator group fold finish) = Aggregator group fold (fmap f finish)
+
+instance A.Applicative (Aggregator a) where
+  pure x = Aggregator (A.pure ()) (A.pure ()) (A.pure x)
+  Aggregator groupf foldf finishf <*> Aggregator groupx foldx finishx =
+    Aggregator (groupf Arr.&&& groupx)
+               ((,) A.<$> foldf A.<*> foldx)
+               (\((rf, rx), (r'f, r'x)) -> finishf (rf, r'f) (finishx (rx, r'x)))
+
+instance PP.ProductProfunctor Aggregator where
+  empty  = PP.defaultEmpty
+  (***!) = PP.defaultProfunctorProduct
+
+aggregate :: Aggregator a b -> Query a -> Query b
+aggregate (Aggregator group fold finish) =
+  Arr.Kleisli
+  . const
+  . map finish
+  . Map.toList
+  . fmap (Foldl.fold fold)
+  . Map.fromListWith (++)
+  . map (group Arr.&&& (:[]))
+  . ($ ())
+  . Arr.runKleisli
+
+groupBy :: Ord a => Aggregator a a
+groupBy = Aggregator id (A.pure ()) fst
+
+fromFoldl :: Foldl.Fold a b -> Aggregator a b
+fromFoldl foldl = Aggregator (A.pure ()) foldl snd
+
+sumA :: Num a => Aggregator a a
+sumA = fromFoldl Foldl.sum
+
+maxA :: Ord a => Aggregator a (Maybe a)
+maxA = fromFoldl Foldl.maximum
+
+countA :: Aggregator a Int
+countA = fromFoldl Foldl.length
+
+test =
+  (runQuery
+   . orderBy (desc fst <> asc snd)
+   . aggregate (PP.p2 (countA, groupBy)))
+  (Arr.arr (\(x, y, _) -> (x, y)) Arr.<<< speakers)
+
+data Order a = Order (a -> a -> Ordering)
+
+asc :: Ord b => (a -> b) -> Order a
+asc f = Order (\x y -> compare (f x) (f y))
+
+desc :: Ord b => (a -> b) -> Order a
+desc f = Order (\x y -> compare (f y) (f x))
+
+orderBy :: Order a -> Query a -> Query a
+orderBy (Order compare') = listQuery .L.sortBy compare' . runQuery
+
+instance M.Monoid (Order a) where
+  mempty = Order (const (const EQ))
+  Order f `mappend` Order g = Order (\x y -> f x y `M.mappend` g x y)
+
+keepWhen :: (a -> Bool) -> QueryArr a a
+keepWhen p = Arr.Kleisli (\a -> if p a then [a] else [])
+
+listQuery :: [a] -> Query a
+listQuery = Arr.Kleisli . const
+
+runQuery :: Query a -> [a]
+runQuery = ($ ()) . Arr.runKleisli
+
+speakers = listQuery [ ("Andres", "DE", "Monday")
+                     , ("Simon PJ", "UK", "Monday")
+                     , ("Alfredo", "IT", "Monday")
+                     , ("Jasper", "BE", "Monday")
+                     , ("Vladimir", "RU", "Monday")
+                     , ("Francesco", "IT", "Monday")
+                     , ("Matthew", "UK", "Monday")
+                     , ("Ivan", "ES", "Monday")
+                     , ("Johan", "SE", "Monday")
+                     , ("Martijn", "NL", "Monday")
+                     , ("Lennart", "SE", "Monday")
+                     , ("Phillip", "DE", "Monday")
+                     , ("Bodil", "NO", "Monday")
+                     , ("San", "BE", "Monday")
+                     , ("Simon M", "UK", "Tuesday")
+                     , ("Neil", "UK", "Tuesday")
+                     , ("Alp", "FR", "Tuesday")
+                     , ("Mietek", "PL", "Tuesday")
+                     , ("Lars", "DE", "Tuesday")
+                     , ("Miles", "UK", "Tuesday")
+                     , ("Gershom", "US", "Tuesday")
+                     , ("Tom", "UK", "Tuesday")
+                     , ("Nicolas", "UK", "Tuesday")
+                     , ("Luite", "NL", "Tuesday") ]
+
+directions = listQuery [ ("DE", "North")
+                       , ("UK", "North")
+                       , ("IT", "South")
+                       , ("BE", "North")
+                       , ("ES", "South")
+                       , ("NL", "North")
+                       , ("RU", "East")
+                       , ("SE", "North")
+                       , ("US", "West")
+                       , ("PL", "East")
+                       , ("NO", "North")
+                       , ("FR", "North") ]
+
+q :: Query (String, String)
+q = proc () -> do
+  (speaker, country, _) <- speakers  -< ()
+  (country', direction) <- directions -< ()
+  restrict -< country == country'
+  Arr.returnA -< (speaker, direction)
+  
+qcount :: [(Int, String)]
+qcount = (runQuery
+          . orderBy (desc fst <> asc snd)
+          . aggregate (PP.p2 (countA, groupBy)))
+         q
