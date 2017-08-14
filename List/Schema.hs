28fdf93aac5f1203e292e1b91d0feca99d0471f0
Tom Ellis <toelli@microsoft.com>
2017-08-14 09:47:53 +0100

    Tom -> Sam

--- List/Schema.hs
+++ List/Schema.hs
@@ -22,7 +22,7 @@ data Manager = Manager {
   deriving Show
 
 employees :: [Employee]
-employees = [ Employee "Tom"   "Back end"  "UK"
+employees = [ Employee "Sam"   "Back end"  "UK"
             , Employee "Neil"  "Back end"  "UK"
             , Employee "Alice" "Front end" "FR"
             , Employee "John"  "Front end" "UK"
@@ -30,8 +30,8 @@ employees = [ Employee "Tom"   "Back end"  "UK"
             , Employee "Ryan"  "Testing"   "FR" ]
 
 output :: [EmployeeOutput]
-output = [ EmployeeOutput "Tom"   "Monday"  "UK" 530
-         , EmployeeOutput "Tom"   "Tuesday" "UK" 320
+output = [ EmployeeOutput "Sam"   "Monday"  "UK" 530
+         , EmployeeOutput "Sam"   "Tuesday" "UK" 320
          , EmployeeOutput "Neil"  "Monday"  "UK" 230
          , EmployeeOutput "Neil"  "Tuesday" "FR" 950
          , EmployeeOutput "Alice" "Monday"  "FR" 630
@@ -44,6 +44,6 @@ output = [ EmployeeOutput "Tom"   "Monday"  "UK" 530
          , EmployeeOutput "Ryan"  "Tuesday" "UK" 380 ]
 
 managers :: [Manager]
-managers = [ Manager "Back end"  "Tom"
+managers = [ Manager "Back end"  "Sam"
            , Manager "Front end" "Alice"
            , Manager "Testing"   "Anne" ]
